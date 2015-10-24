f794370c9dfbfeba4624413867be48108c55a3ac
Patrick Arlt <patrick.arlt@gmail.com>
2015-10-24 13:05:38 -0700

    fix jsnext main

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export attribute from './src/attribute.js';
-export bindEvent from './src/bindEvent.js';
-export watchAttribute from './src/watchAttribute.js';
-export rivetsTemplate from './src/rivetsTemplate.js';
+export { default as attribute} from './src/attribute.js';
+export { default as bindEvent } from './src/bindEvent.js';
+export { default as watchAttribute } from './src/watchAttribute.js';
+export { default as rivetsTemplate } from './src/rivetsTemplate.js';
